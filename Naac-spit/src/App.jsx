508cc0560c7008f3f7d33c5a0c601814e8a4bdc3
RUSHIKESH KHEDEKAR <rxshii09@gmail.com>
2024-01-05 15:12:45 +0530

    new changes

--- Naac-spit/src/App.jsx
+++ Naac-spit/src/App.jsx
@@ -1,13 +1,30 @@
 import Buttons from "./components/Buttons.jsx";
-import Header from "./components/Header.jsx";
-import CriteriaPage from "./pages/CriteriaPage";
+import { motion } from 'framer-motion';
 
 function App() {
+  const Btns=[
+    "CRITERIA 1",
+    "CRITERIA 2",
+    "CRITERIA 3",
+    "CRITERIA 4",
+    "CRITERIA 5",
+    "CRITERIA 6",
+    "CRITERIA 7",
+    "SSR",
+  ]
   return (
     <>
-    <Header/>
-    <Buttons/>
+    {/* <Header/> */}
+    <motion.div className="flex gap-3 justify-center align-center">
+    {Btns.map((content, index) => (
+        <div key={index}>
+          <Buttons Btn={content} />
+        </div>
+      ))}
+      </motion.div>
+    {/* <div className="mt-3">
     <CriteriaPage/>
+    </div> */}
     </>
   );
 } 
